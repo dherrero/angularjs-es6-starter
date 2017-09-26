f5a030d85c68e0739bca13505d5b79ba7ef64ec5
dherrero <dherrero@alfatecsistemas.es>
2017-09-26 16:08:05 +0200

    more test examples

--- src/app/components/comp-example/comp-example.spec.js
+++ src/app/components/comp-example/comp-example.spec.js
@@ -1,15 +1,30 @@
 describe('Component compExample', () => {
     var component;
+    var $document;
 
     beforeEach(() => {
         angular.mock.module('app');        
     });
 
-    beforeEach(inject((_$componentController_) => {
+    beforeEach(inject((_$componentController_, _$document_) => {
         component = _$componentController_('compExample');
+        $document = _$document_;
     }));
 
-    it('compExample should exists', () => {
-        expect(component).toBeDefined();
+    afterEach(() => {
+        $document.find('#toast-container').remove();
+    });
+
+    describe('basic scenarios', () => {
+        it('should exists', () => {
+            expect(component).toBeDefined();
+        });
+
+        it('should show toast when click button error', () =>{
+            component.toast('error');
+            setTimeout(()=>{
+                expect($('#toast-container .toast-error').length > 0).toBe(true);
+            },500);
+        });
     });
 });
--- src/app/filters/capitalize.spec.js
+++ src/app/filters/capitalize.spec.js
@@ -8,8 +8,13 @@ describe('Filter: capitalize', () => {
        capitalize = _capitalizeFilter_;
     }));
 
-    it('can capitalize words', function() {
-        expect(capitalize('hello')).toBe('Hello');
-        expect(capitalize('hello world')).toBe('Hello World');
+    describe('basic scenarios', () => {
+        it('can capitalize strings', function() {
+            expect(capitalize('hello')).toBe('Hello');
+            expect(capitalize('hello world')).toBe('Hello World');
+            expect(capitalize('HELLO WORLD')).toBe('Hello World');
+            expect(capitalize('hElLo WoRLD')).toBe('Hello World');
+        });
     });
+
   });
